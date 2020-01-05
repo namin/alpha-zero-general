cf4b1330068af6a8d81d6c28e6dd7d45d643b15b
Nada Amin <namin@alum.mit.edu>
2020-01-05 13:28:37 +0100

    add a simple static strategy

--- _chess/ChessPlayers.py
+++ _chess/ChessPlayers.py
@@ -39,3 +39,63 @@ class HumanChessPlayer():
         if board.turn:
             move = mirror_move(move)
         return from_move(move)
+
+class StrategicChessPlayer():
+    def __init__(self, game, strategy):
+        self.strategy = strategy
+
+    def play(self, board):
+        mboard = board
+        if board.turn:
+            mboard = board.mirror()
+        move = self.strategy(mboard)
+        if board.turn:
+            move = mirror_move(move)
+        return from_move(move)
+
+MIN_SCORE = -1000
+MAX_SCORE = -MIN_SCORE
+
+piece_values = {chess.KING: 0,
+                chess.PAWN: 1,
+                chess.BISHOP: 3,
+                chess.KNIGHT: 3,
+                chess.ROOK: 5,
+                chess.QUEEN: 9}
+
+def evaluate_board(board):
+    score = 0
+    for piece in board.piece_map().values():
+        value = piece_values[piece.piece_type]
+        factor = 1 if piece.color == board.turn else -1
+        score += factor * value
+    score -= 100 if board.is_checkmate() else 0
+    return score
+
+def evaluate_move(move, board, evaluate_board):
+    board.push(move)
+    score = -evaluate_board(board)
+    board.pop()
+    return score
+
+def evaluation_strategy(board, evaluate_move):
+    best_moves = []
+    best_score = MIN_SCORE
+    for move in board.legal_moves:
+        score = evaluate_move(move, board)
+        if score > best_score:
+           best_score = score
+           best_moves = [move]
+        elif score == best_score:
+           best_moves.append(move)
+    return random.choice(best_moves)
+
+def static_evaluate_move(move, board):
+    return evaluate_move(move, board, evaluate_board)
+
+def static_strategy(board):
+    return evaluation_strategy(board, static_evaluate_move)
+
+class StaticChessPlayer(StrategicChessPlayer):
+    def __init__(self, game):
+        super().__init__(game, static_strategy)
--- pit.py
+++ pit.py
@@ -1,7 +1,8 @@
 import Arena
 from MCTS import MCTS
 from _chess.ChessGame import ChessGame as Game
-from _chess.ChessPlayers import HumanChessPlayer as HumanPlayer, RandomPlayer
+from _chess.ChessPlayers import HumanChessPlayer as HumanPlayer, RandomPlayer, StaticChessPlayer
+
 from _chess.pytorch.NNet import NNetWrapper as NNet
 
 import numpy as np
@@ -11,21 +12,27 @@ from utils import *
 use this script to play manually with the best temp agent.
 """
 
+best_static = True
 human_vs_cpu = False
 
 g = Game()
 
 hp = HumanPlayer(g).play
 rp = RandomPlayer(g).play
+sp = StaticChessPlayer(g).play
 
+try:
+  # nnet players
+  n1 = NNet(g)
+  n1.load_checkpoint('./temp/', 'best.pth.tar')
+  args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+  mcts1 = MCTS(g, n1, args1)
+  ap = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+except ValueError:
+  print('warning: no AI found')
+  ap = None
 
-# nnet players
-n1 = NNet(g)
-n1.load_checkpoint('./temp/', 'best.pth.tar')
-args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
-mcts1 = MCTS(g, n1, args1)
-player1 = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
-
+player1 = sp if best_static else ap
 player2 = hp if human_vs_cpu else rp
 
 arena = Arena.Arena(player1, player2, g, display=Game.display)
